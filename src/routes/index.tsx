6810014ca791ae4fddab733ffdcd8fcaa2b68bbf
Thiago Dantas <thiago.dantas@stone.com.br>
2024-07-27 18:34:56 -0300

    feat: add mui design system and setting theme light

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@mui/material";
 import { Route, Routes, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="pagina-inicial" element={<p>Página inicial</p>} />
+      <Route
+        path="pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            TESTE
+          </Button>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
