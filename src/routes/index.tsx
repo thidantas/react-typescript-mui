f2b042111dd53ce3751b04e1420f7340aef4268c
Thiago Dantas <thiago.dantas@stone.com.br>
2024-07-27 17:56:08 -0300

    feat: setting routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {};
+import { Route, Routes, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="pagina-inicial" element={<p>Página inicial</p>} />
+
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
